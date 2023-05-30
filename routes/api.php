a829eccb483bb325e83d8587b22901859beaae39
NiazAhmedNayeem <niazahmed.net@gmail.com>
2023-05-30 23:37:06 +0600

    Update

--- routes/api.php
+++ routes/api.php
@@ -20,20 +20,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 ///GET API testing
 Route::get('/people/{id?}', [UserApiController::class, 'showPeople']);
+
 ///POST API testing
 Route::post('/add-people', [UserApiController::class, 'addPeople']);
+
 ///POST API multiple people add testing
 Route::post('/add-multiple-people', [UserApiController::class, 'addMultiplePeople']);
+
 ///PUT API for update people detail
 Route::put('/update-people-detail/{id}', [UserApiController::class, 'updatePeopleDetail']);
+
 ///PATCH API for update single record
 Route::patch('/update-single-record/{id}', [UserApiController::class, 'updateSingleRecord']);
+
 ///DELETE API for delete single user or people
 Route::delete('/delete-user-record/{id}', [UserApiController::class, 'deleteUserRecord']);
+
 ///DELETE API for delete single user or people with json
 Route::delete('/delete-user-record-with-json', [UserApiController::class, 'deleteUserRecordWithJson']);
+
 ///DELETE API for delete multiple user or people
 Route::delete('/delete-multiple-user-record/{ids}', [UserApiController::class, 'deleteMultipleUserRecord']);
+
 ///DELETE API for delete multiple user or people with json
 Route::delete('/delete-multiple-user-record-with-json', [UserApiController::class, 'deleteMultipleUserRecordWithJson']);
 
