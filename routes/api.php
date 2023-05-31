c02688cace7e62c484756593dae70c8886187e6f
NiazAhmedNayeem <niazahmed.net@gmail.com>
2023-05-31 16:38:14 +0600

    Install Laravel/passport and kye generate

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\UserApiController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 ///GET API testing
 Route::get('/people/{id?}', [UserApiController::class, 'showPeople']);
 
